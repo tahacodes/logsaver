4ef2a7bb93e242e1c595eee5c5bb7ec1e96cb50d
Taha Farahani <iotahacodes@gmail.com>
2021-02-23 12:37:45 +0330

    insert data to MongoDB

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
+from flask import Flask
+import pymongo
+
+app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        host="localhost", port=27017, serverSelectionTimeoutMS=1000)
+    db = mongo.demo
+    mongo.server_info()
+except:
+    print("Couldn't connect to MongoDB")
+
+
+@app.route('/', methods=['GET'])
+def get():
+    return
+
+
+@app.route('/', methods=['POST'])
+def post():
+    try:
+        log = {
+            "status": 200,
+            "timestamp": 1614033055,
+            "output": "CREATED"
+        }
+        dbResponse = db.logs.insert_one(log)
+        for attr in dir(dbResponse):
+            print(attr)
+    except Exception as ex:
+        print(ex)
+
+
 if __name__ == '__main__':
-    print("im a log saver")
+    app.run(port=8080, debug=False)
